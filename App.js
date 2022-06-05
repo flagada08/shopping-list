8f8bf646119e7bc467a224368ff27e3a70e138df
flagada08 <flagada08@gmail.com>
2022-06-05 19:40:43 +0200

    AddProduct components

--- App.js
+++ App.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import Products from './components/Products';
+import AddProduct from './components/AddProduct';
 
 export default function App() {
 
-  const [product, setProduct] = useState('');
   const [myProducts, setMyProducts] = useState([]);
 
-  const inputHandler = (val) => {
-    setProduct(val)
-  }
-
-  const submitHandler = () => {
+  const submitHandler = (product, setProduct) => {
     // console.log(product);
     // setMyProducts([...myProducts, product])
     const idString = Date.now().toString();
@@ -24,19 +20,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-          style={styles.textInput}
-          placeholder="Nouveau produit"
-          onChangeText={inputHandler}
-          value={product}
-        />
-        <Button 
-          title="VALIDER"
-          onPress={submitHandler}
-        />
-      </View>
-
+      <AddProduct submitHandler={submitHandler}/>
       <FlatList
         data={myProducts}
         renderItem={({item}) => <Products name={item.name}/>}
@@ -58,17 +42,5 @@ const styles = StyleSheet.create({
   container: {
     padding: 40,
     paddingTop: 60    
-  },
-  inputContainer: {
-    flexDirection: "row",
-    marginBottom: 9
-  },
-  textInput: {
-    borderColor: "grey",
-    borderWidth: 1,
-    padding: 5,
-    paddingLeft: 9,
-    fontSize: 18,
-    flexGrow: 1
-  },
+  }
 });
---
+++ components/AddProduct.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, TextInput,Button } from 'react-native';
+
+const AddProduct = ({submitHandler}) => {
+
+  const [product, setProduct] = useState('');
+
+  const inputHandler = (val) => {
+    setProduct(val)
+  }
+
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput 
+                style={styles.textInput}
+                placeholder="Nouveau produit"
+                onChangeText={inputHandler}
+                value={product}
+            />
+            <Button 
+                title="VALIDER"
+                onPress={() => submitHandler(product, setProduct)}
+            />
+      </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      flexDirection: "row",
+      marginBottom: 9
+    },
+    textInput: {
+      borderColor: "grey",
+      borderWidth: 1,
+      borderRadius: 3,
+      padding: 5,
+      paddingLeft: 9,
+      fontSize: 18,
+      flexGrow: 1
+    },
+  });
+
+export default AddProduct;
